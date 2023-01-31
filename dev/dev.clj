b4cb6e3822fe059f6fbeebbc0f2b1fc5738797e0
Igor Kislitsyn <k.yngvarr@gmail.com>
2023-02-01 04:14:24 +0700

    added a helper function for quick db population

--- dev/dev.clj
+++ dev/dev.clj
@@ -2,15 +2,33 @@
   (:require [figwheel.main]
             [figwheel.main.api]
             [ring.adapter.jetty :refer [run-jetty]]
-            [health.backend :refer [app]]))
+            [health.backend :refer [app]]
+            [health.database :as db]
+            [next.jdbc.types :refer [as-date]]))
 
 (defn cljs []
   (if (get @figwheel.main/build-registry "dev")
     (figwheel.main.api/cljs-repl "dev")
     (figwheel.main.api/start "dev")))
 
+(defn random-name []
+  (str (rand-nth ["Fabio" "Luca" "Sascha" "Alex" "Daniele"]) " "
+       (rand-nth ["Lione" "Turilli" "Paeth" "Staropoli" "Carbonera"])))
+
+(defn random-date []
+  (as-date (str (+ 1910 (rand-int 100)) "-" (+ 1 (rand-int 11)) "-" (+ 1 (rand-int 27)))))
+
+(defn populate-db [amount]
+  (doseq [x (range amount)]
+    (db/add-patient {:fullname (random-name)
+                     :gender (rand-nth ["male" "female" "other"])
+                     :birthdate (random-date)
+                     :address (rand-nth ["Finland" "Real World"])
+                     :insurancenum (+ 100000 (rand-int 999999999))})))
+
 (comment
   (def server (run-jetty app {:port 8080 :join? false}))
   (.stop server)
+  (populate-db 10)
   (cljs)
   )
